d0cf664da42816392459c68a80dc2d0bca762b62
Antoni <antoni2ee@gmail.com>
2019-09-07 23:10:34 +0200

    new version of byte buddy

--- src/main/java/com/github/antoni/bytebuddy/Main.java
+++ src/main/java/com/github/antoni/bytebuddy/Main.java
@@ -15,6 +15,16 @@ public class Main {
         System.out.println("number = [" + number + "], name = [" + name + "], bool = [" + bool + "]");
     }
 
+
+
+    public static void localVariables() throws Exception {
+        int i = 1;
+        String s = "zzz";
+        System.out.println("s = " + s);
+        https://stackoverflow.com/questions/44684442/byte-buddy-stack-manipulation-how-to-work-with-local-variables-and-if-statement
+        System.out.println("i = " + i);
+    }
+
     public static void main(String[] args) throws Exception {
         method4(1,"Ludwik", true);
         System.out.println("methods="+Arrays.stream(Main.class.getDeclaredMethods()).map(Method::getName).collect(Collectors.joining(",")));
